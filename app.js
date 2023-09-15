004fbdff1141fe97656702e7450d9c7556af5f43
Gvistic <gvistic@gmail.com>
2023-09-14 22:46:00 -0700

    Switched to RoProxy for hopefully better rate limits.

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/bundles/user/:userId', (req, res) => {
 	const userId = req.params.userId
 	const { limit, cursor } = req.query
 
-	const url = `https://catalog.roblox.com/v1/users/${userId}/bundles?limit=${limit}&sortOrder=Asc`
+	const url = `https://catalog.roproxy.com/v1/users/${userId}/bundles?limit=${limit}&sortOrder=Asc`
 	if (cursor) url += `&cursor=${cursor}`
 	request(url, (err, response, body) => {
 		if (err) {
@@ -21,6 +21,7 @@ app.get('/', (_req, res) => {
 	res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+app.listen(port, '0.0.0.0', () => {
+	console.log(`App listening on port ${port}`)
+});
+  
